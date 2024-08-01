f84476d09b35af755718b26a5d7ececfeb3976e8
Byung jin <bjcho1503@naver.com>
2024-08-01 14:25:49 +0900

    BrowserRouter 위치 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import WeatherInfo from "./pages/weatherInfo/WeatherInfo";
 import WeatherSelect from "./pages/weatherSelect/WeatherSelect";
 import NotFound from "./pages/notFound/NotFound";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route
-                    path="/"
-                    exact={true}
-                    element={<WeatherSelect />}
-                />
-                <Route
-                    path="/detail/:location"
-                    element={<WeatherInfo />}
-                />
-                <Route
-                    path="*"
-                    element={<NotFound />}
-                />
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route
+                path="/"
+                exact={true}
+                element={<WeatherSelect />}
+            />
+            <Route
+                path="/detail/:location"
+                element={<WeatherInfo />}
+            />
+            <Route
+                path="*"
+                element={<NotFound />}
+            />
+        </Routes>
     );
 }
 
